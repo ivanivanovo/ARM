a7fc75e875bd210920db5443b39a6c2f83866659
ivanivanovo <ivanivanovo@mail.ru>
2021-04-20 19:06:19 +0300

    цепочка мнемоник

--- langASM.f
+++ langASM.f
@@ -53,8 +53,8 @@ CREATE pre fields_of_mnemonic ALLOT
  FALSE pre .Set  !
      ;
 
-\ Чтобы на стеке отличить номер регистра от числа
-\ под номер регистра подкладывается специальный признак
+\ Регистр представлен на стеке парой чисел:
+\ признак регистра & номер регистра
 129 CONSTANT itisReg \ признак регистра
 : REGISTER: ( n <name> --) \ слово определяющее регистр
     CREATE , itisReg , 
@@ -66,11 +66,50 @@ CREATE pre fields_of_mnemonic ALLOT
 12 REGISTER: R12   13 REGISTER: R13   14 REGISTER: R14    15 REGISTER: R15
                    13 REGISTER: SP    14 REGISTER: LR     15 REGISTER: PC
 
+0
+CELL -- .next       \ указатель на следующий член списка
+CELL -- .operands   \ указатель на поле операндов
+   1 -- .mnemo      \ строка со счетчиком, мнемоника
+CONSTANT mnemonics_struct
 
-: Assm: ( <mem> --) \ начинает цепочку связанных полей для описания 
-    \ ассемблерной команда .mem  
+VARIABLE Mnemonics  \ кончик списка мнемоник
+0 Mnemonics ! \ список пуст
+
+: search_Mnemo ( adr u -- link) \ искать такую же мнемонику
+    \ если нет, то вернуть 0
+    2>R
+    Mnemonics @
+    BEGIN DUP 
+    WHILE DUP .mnemo COUNT 2R@ COMPARE
+    WHILE @ REPEAT 
+    THEN
+    2R> 2DROP
+    ;
+
+: addMnemonics ( adr u -- link) \ 
+    Mnemonics @ HERE Mnemonics ! , 0 , str! 
+    ALIGN
+    Mnemonics @
+    ;
+
+: Assm: ( <mnemonics> -- link) \ начинает цепочку связанных полей для описания 
+    \ ассемблерной команда <mnemonics>
+    >IN @  BL PARSE 2>R \ adr u - мнемоника 
+           2R@ UPPERCASE-W \ ВСЕГДА В ВЕРХНЕМ РЕГИСТРЕ
+           2R@ search_Mnemo
+    DUP 0= 
+    IF DROP >IN ! CREATE \ нету, создать
+       2R@ addMnemonics  \ добавить в цепь
+    ELSE NIP THEN
+    2R> 2DROP
     ;
 
 
-PREVIOUS DEFINITIONS    
+\ PREVIOUS DEFINITIONS    
 #def langASM .( loaded) CR
+
+Assm: a1
+\ EOF
+Assm: b1
+Assm: a2
+Assm: b1
