cb92837f5902b2550e5c581522694bfbe89a47ff
ivanivanovo <ivanivanovo@mail.ru>
2021-04-21 13:37:58 +0300

    мнемоники как слова

--- langASM.f
+++ langASM.f
@@ -66,50 +66,42 @@ CREATE pre fields_of_mnemonic ALLOT
 12 REGISTER: R12   13 REGISTER: R13   14 REGISTER: R14    15 REGISTER: R15
                    13 REGISTER: SP    14 REGISTER: LR     15 REGISTER: PC
 
-0
-CELL -- .next       \ указатель на следующий член списка
-CELL -- .operands   \ указатель на поле операндов
-   1 -- .mnemo      \ строка со счетчиком, мнемоника
-CONSTANT mnemonics_struct
 
-VARIABLE Mnemonics  \ кончик списка мнемоник
-0 Mnemonics ! \ список пуст
 
-: search_Mnemo ( adr u -- link) \ искать такую же мнемонику
-    \ если нет, то вернуть 0
-    2>R
-    Mnemonics @
-    BEGIN DUP 
-    WHILE DUP .mnemo COUNT 2R@ COMPARE
-    WHILE @ REPEAT 
-    THEN
-    2R> 2DROP
+DEFER coder ( link --) \ кодировщик, переводит ассемблерную строку в машинный код
+
+\ структура мнемоники
+\ fop   - указатель на поле операндов
+\ c-str - мнемоника, строка со счетчиком
+
+: Assm: ( "mnemonics" -- link) \ создает или находит структуру 
+    \ ассемблерной команды <mnemonics>
+    \ возвращает ссылку на неё
+    >IN @  BL PARSE 2>R \ R: adr u - мнемоника во входном буфере
+    2R@ UPPERCASE-W \ ВСЕГДА В ВЕРХНЕМ РЕГИСТРЕ
+    2R@ GET-CURRENT SEARCH-WORDLIST  
+    IF  >BODY  
+        NIP 2R> 2DROP 
+    ELSE \ нету, создать
+        >IN ! CREATE    
+        HERE 0 , 2R> str! ALIGN
+        DOES> coder
+    THEN 
     ;
 
-: addMnemonics ( adr u -- link) \ 
-    Mnemonics @ HERE Mnemonics ! , 0 , str! 
-    ALIGN
-    Mnemonics @
+: cod? ( link -- )
+    DUP @ .HEX CELL + COUNT TYPE CR
     ;
+' cod? IS coder
 
-: Assm: ( <mnemonics> -- link) \ начинает цепочку связанных полей для описания 
-    \ ассемблерной команда <mnemonics>
-    >IN @  BL PARSE 2>R \ adr u - мнемоника 
-           2R@ UPPERCASE-W \ ВСЕГДА В ВЕРХНЕМ РЕГИСТРЕ
-           2R@ search_Mnemo
-    DUP 0= 
-    IF DROP >IN ! CREATE \ нету, создать
-       2R@ addMnemonics  \ добавить в цепь
-    ELSE NIP THEN
-    2R> 2DROP
-    ;
-
-
-\ PREVIOUS DEFINITIONS    
+PREVIOUS DEFINITIONS    
 #def langASM .( loaded) CR
 
-Assm: a1
-\ EOF
-Assm: b1
-Assm: a2
-Assm: b1
+\EOF \ локальные отладочные тесты
+Assm: a1  DROP
+Assm: b1  DROP
+Assm: a2  DROP
+Assm: a2  DROP
+Assm: B1  DROP
+
+WORDS
