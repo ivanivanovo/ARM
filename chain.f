c24c3b55b6e01053a0f33e031b03c311edefd4df
ivanivanovo <ivanivanovo@mail.ru>
2021-04-29 12:14:48 +0300

    list

--- chain.f
+++
@@ -1,7 +0,0 @@
-\ связанные списки
-: NewChain ( <name> --) \ создать новую цепочку с именем <name>
-    CREATE 0 , 0 ,
-    ;
-: inject ( adr1 u1 chain --) \ вставить объект с именем adr1 u1 в цепь chain
-    . TYPE CR
-    ;
